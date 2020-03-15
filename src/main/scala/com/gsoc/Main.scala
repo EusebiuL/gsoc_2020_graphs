9a0befaaa26ccfac471a657457659031a2e9d730
Eusebiu Lazar <lazardeniseusebiu@gmail.com>
2020-03-15 23:30:21 +0200

    Implement main method

--- src/main/scala/com/gsoc/Main.scala
+++ src/main/scala/com/gsoc/Main.scala
@@ -1,6 +1,30 @@
 package com.gsoc
 
+import com.gsoc.gremlin.GremlinGraph
+import com.gsoc.model.Model
+import com.gsoc.processor.AlertsProcessor
+import org.apache.commons.configuration.PropertiesConfiguration
 
-object Main extends App {
-  println("Hello, World!")
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+
+    for {
+      conf <- Future.fromTry {
+        Try {
+          new PropertiesConfiguration("remote-graph.properties")
+        }
+      }.recoverWith {
+        case e: Throwable => throw new RuntimeException(s"Exception when reading config file: ${e.getLocalizedMessage}")
+      }
+
+      graph = GremlinGraph[Model](conf)
+      processor = new AlertsProcessor
+    } yield processor.startProcessor[Model](graph)
+  }
+
 }
