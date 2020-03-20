141b1ab6ba3bd033efcbfd9bda3a269fe3bc4713
Eusebiu Lazar <lazardeniseusebiu@gmail.com>
2020-03-20 19:56:45 +0200

    Change main

--- src/main/scala/com/gsoc/Main.scala
+++ src/main/scala/com/gsoc/Main.scala
@@ -1,20 +1,24 @@
 package com.gsoc
 
-import com.gsoc.gremlin.GremlinGraph
-import com.gsoc.model.{Alert, Model}
+import com.gsoc.gremlin_graph.GremlinGraph
+import com.gsoc.models.Alert
 import com.gsoc.processor.AlertsProcessor
 import org.apache.commons.configuration.PropertiesConfiguration
+import org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph
+import org.slf4j.{Logger, LoggerFactory}
+import gremlin.scala._
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+    val logger: Logger = LoggerFactory.getLogger(getClass)
 
-    for {
-      conf <- Future.fromTry {
+    val result = for {
+      conf <-  Future.fromTry {
         Try {
           new PropertiesConfiguration("remote-graph.properties")
         }
@@ -22,9 +26,14 @@ object Main {
         case e: Throwable => throw new RuntimeException(s"Exception when reading config file: ${e.getLocalizedMessage}")
       }
 
-      graph = GremlinGraph[Alert](conf)
-      processor = new AlertsProcessor
-    } yield processor.startProcessor[Alert](graph)(Alert.alertReads)
+      graph =  GremlinGraph[Alert](conf)
+      processor = new AlertsProcessor()
+      result <- processor.startProcessor[Alert](graph)(Alert.alertReads)
+    } yield result
+    result.onComplete  {
+      case Success(value) => logger.info(value.traversal.V.valueMap().toString())
+      case Failure(exception) => throw new RuntimeException(exception.getLocalizedMessage)
+    }
   }
 
 }
